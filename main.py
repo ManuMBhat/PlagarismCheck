5da68c8d0aea22d91518597469e34692615cfce5
pronei <pranay.mundra@outlook.com>
2020-08-08 14:35:00 +0530

    add stemmer, remove loop for dot products

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-from utils import parse, twoDocs, traverseDir
+from utils import parse, DocumentPair, traverseDir
 
 def main():
     args = parse()
+    flags = list((args.csv, args.xlsx, args.html))
     if not args.dir:
         if args.doc1 is None or args.doc2 is None:
             print("No doc entered")
             exit(0)
-        print(twoDocs(args.doc1,args.doc2))
+        twoDocs = DocumentPair(args.doc1, args.doc2)
+        print(twoDocs.dotProduct())
     else:
         if not (args.doc1 is None and args.doc2 is None):
             print("Ignoring mentioned doc and working on dir")
-        finalOutput = traverseDir(args.dir, args.threshold)
+        finalOutput = traverseDir(args.dir, flags, args.threshold)
 
 if __name__ == "__main__":
     main()
--- utils.py
+++ utils.py
@@ -1,67 +1,79 @@
 import numpy as np
 import pandas as pd
+from stemmer import PorterStemmer
 import argparse
 import os
 import csv
 
-outputFile = "PlagarismText"
+outputFile = "Plagarised"
 
-def docToDict(filename):
-    words = {}
-    with open(filename) as infile:
-        for line in infile:
-            line = line.strip()
-            word_list = line.split()
-            for word in word_list:
-                if word in words.keys():
-                    words[word] += 1
-                else:
-                    words[word] = 1
-    return words
+class DocumentPair:
 
+    def __init__(self, fileA, fileB):
+        self.__allWords = set()
+        self.__wordsA = dict()
+        self.__wordsB = dict()
 
-def dotProduct(words1, words2):
-    
-    all_words = list(words1.keys())
-    all_words.extend(list(words2.keys()))
-    
-    dot_product = 0
-    word1_norm = 0
-    word2_norm = 0
-    words1_keys = words1.keys()
-    words2_keys = words2.keys()
-    for word in all_words:
-        w1_val = 0
-        w2_val = 0
-        if word in words1_keys:
-            w1_val = words1[word]
-        if word in words2_keys:
-            w2_val = words2[word]
-        dot_product += (w1_val * w2_val)
-        word1_norm += w1_val ** 2
-        word2_norm += w2_val ** 2
-    word1_norm = word1_norm ** 0.5
-    word2_norm = word2_norm ** 0.5
+        with open(fileA, 'r') as document:
+            for line in document:
+                words = line.strip().split()
+                for word in words:
+                    p = PorterStemmer()
+                    word = p.stem(word, 0, len(word)-1)
+                    if word in self.__wordsA.keys():
+                        self.__wordsA[word] += 1
+                    else :
+                        self.__wordsA[word] = 1
 
-    return dot_product/(word1_norm * word2_norm)
+        with open(fileB, 'r') as document:
+            for line in document:
+                words = line.strip().split()
+                for word in words:
+                    p = PorterStemmer()
+                    word = p.stem(word, 0, len(word)-1)
+                    if word in self.__wordsB.keys():
+                        self.__wordsB[word] += 1
+                    else :
+                        self.__wordsB[word] = 1
+
+        self.__allWords = set(self.__wordsA.keys()) | set(self.__wordsB.keys())
+        self.__table = {t[1] : t[0] for t in enumerate(self.__allWords)}
+
+    def getAllWords(self):
+        return self.__allWords
+
+    def dotProduct(self):
+        wordMat = np.zeros((2, len(self.__allWords)))
+
+        for key in self.__wordsA:
+            wordMat[0, self.__table[key]] = self.__wordsA[key]
+        for key in self.__wordsB:
+            wordMat[1, self.__table[key]] = self.__wordsB[key]
+
+        dot = wordMat[0].dot(wordMat[1])
+        normA = np.linalg.norm(wordMat[0])
+        normB = np.linalg.norm(wordMat[1])
+
+        return dot / (normA * normB)
 
 
 def parse():
     parser = argparse.ArgumentParser()
     #flag for directory
     parser.add_argument("--dir", default=False)
-    parser.add_argument("--doc1",default=None)
-    parser.add_argument("--doc2",default=None)
-    parser.add_argument("--out_csv",default=outputFile)
-    parser.add_argument("--threshold",default=0.0, type=float)
+    parser.add_argument("--doc1", default=None)
+    parser.add_argument("--doc2", default=None)
+    parser.add_argument("--out", default=outputFile)
+    parser.add_argument("--csv", action='store_true')
+    parser.add_argument("--xlsx", action='store_true')
+    parser.add_argument("--html", action='store_true')
+    parser.add_argument("--threshold", default=0.0, type=float)
     
     return parser.parse_args()
 
 
-def traverseDir(dirname, threshold):
-    docNames = [os.path.join(dirname,f) for f in os.listdir(dirname) if f[-4:] == ".txt"]
-    print(docNames)
-    docWords = [docToDict(f) for f in docNames]
+def traverseDir(dirname, flags, threshold):
+    docNames = [os.path.join(dirname, f) for f in os.listdir(dirname) if f[-4:] == ".txt"]
     
     finalMatrix = np.empty((len(docNames), len(docNames)))
     finalMatrix[:] = np.nan
@@ -71,15 +83,20 @@ def traverseDir(dirname, threshold):
     for i in range(len(docNames)):
         j = i + 1
         while j < len(docNames):
-            result = dotProduct(docWords[i],docWords[j])
-            finalOutput.append([docNames[i],docNames[j],result])
+            pair = DocumentPair(docNames[i], docNames[j])
+            result = pair.dotProduct()
+            finalOutput.append([docNames[i], docNames[j], result])
             finalMatrix[j, i] = result
             j += 1
+    
     print(finalOutput)
 
-    #toCSV(docs, finalMatrix)
-    #toXLSX(docNames, finalMatrix, threshold)
-    toHTML(docNames, finalMatrix, threshold)
+    if flags[0]:
+        toCSV(docNames, finalMatrix)
+    if flags[1]:
+        toXLSX(docNames, finalMatrix, threshold)
+    if flags[2]:
+        toHTML(docNames, finalMatrix, threshold)
 
     return finalOutput
 
@@ -114,10 +131,3 @@ def toCSV(docs, finalMatrix):
         outputwriter.writerow(['File'] + docs)
         for i in range(len(docs)):
             outputwriter.writerow([docs[i]] + list(finalMatrix[i, :]))
-
-
-def twoDocs(filename1,filename2):
-    words1 = docToDict(filename1)
-    words2 = docToDict(filename2)
-
-    return dotProduct(words1,words2)
